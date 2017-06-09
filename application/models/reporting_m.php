a41089ad22b3ebe64628d830b44357c9378f5a12
Jonathan Brown <jonathan@resultsathand.com>
2017-06-09 16:59:19 -0400

    Fixing this problem

--- application/models/reporting_m.php
+++ application/models/reporting_m.php
@@ -80,7 +80,7 @@ class Reporting_M extends Master_M {
     }
 
     public function getRevenueWithinDateRange($start_date_time, $end_date_time) {
-        $query = $this->db->query("Select sum(sales_price) as cnt from `order` join (select distinct order_id from order_status where status = 'Approved') order_status on `order`.id = order_status.order_id where order_date >= unix_timestamp(?) and order_date <= unix_timestamp(?) ", array($start_date_time, $end_date_time));
+        $query = $this->db->query("Select sum(order_transaction.amount) as cnt from `order` join (select distinct order_id from order_status where status = 'Approved') order_status on `order`.id = order_status.order_id join order_transaction on `order`.id = order_transaction.order_id where order_date >= unix_timestamp(?) and order_date <= unix_timestamp(?) ", array($start_date_time, $end_date_time));
         $cnt = 0;
         foreach ($query->result_array() as $row) {
             $cnt = $row['cnt'];
