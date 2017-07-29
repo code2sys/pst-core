7830dcd92aa1d31396722a008dfa36d00f7c0f7e
Jonathan Brown <jonathan@resultsathand.com>
2017-07-29 09:00:55 -0400

    Here

--- application/models/cron/cronjobminute.php
+++ application/models/cron/cronjobminute.php
@@ -15,6 +15,8 @@ class CronJobMinute extends AbstractCronJob
 	{
 		$this->load->model('mail_queue_m');
 		$this->mail_queue_m->processMailQueue($limit);
+        $this->load->model("ebay_m");
+        $this->ebay_m->getOrders();
 	}
 
     protected function procParts($limit = 500)
