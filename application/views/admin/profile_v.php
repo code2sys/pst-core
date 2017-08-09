a9f2849601bcb3683d3ce7995027a62278e2f3ed
Jonathan Brown <jonathan@resultsathand.com>
2017-08-09 11:01:26 -0400

    Here

--- application/views/admin/profile_v.php
+++ application/views/admin/profile_v.php
@@ -332,20 +332,6 @@ if (!defined("ENABLE_OEMPARTS_BUTTON")) {
 											<img src="<?php echo base_url().'assets/benz_assets/remarketing.jpg';?>" width="233px" border="0"/>
 										</td>
 									</tr>
-									<tr>
-										<td style="width:30%;"><b>Google Conversion ID :</b></td>
-										<td><?php echo form_input(array('name' => 'google_conversion_id', 
-										  'value' => @$address['google_conversion_id'], 
-										  'class' => 'text large',
-										  'placeholder' => 'ID')); ?></td>
-									</tr>
-									<tr>
-										<td style="width:30%;"><b>Google Conversion Label :</b></td>
-										<td><?php echo form_input(array('name' => 'google_conversion_label',
-										  'value' => @$address['google_conversion_label'],
-										  'class' => 'text large',
-										  'placeholder' => 'Label')); ?></td>
-									</tr>
 								</table>
 							</td>
 						</tr>
