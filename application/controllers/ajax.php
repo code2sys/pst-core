21fef1ddb119ac48cbabfb246763ac457c6aafcf
Jonathan Brown <jonathan@resultsathand.com>
2017-08-24 14:40:53 -0400

    Can we fix Brandt #4 and #6 for eBay?

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -770,7 +770,7 @@ class Ajax extends Master_Controller {
 		if ($this->ebay_m->updateEbayTracking($post['ebay_id'], $post['ship_tracking_code'], $post['carrier'], $result["error_message"])) {
 		    $result["success"] = true;
             // JLB 08-24-17 - we need to record the tracking number...
-            $results["ship_tracking_code"] = $this->admin_m->updateOrderTrackingNumber($post);
+            $result["ship_tracking_code"] = $this->admin_m->updateOrderTrackingNumber($post);
         }
         print json_encode($result);
 	}
