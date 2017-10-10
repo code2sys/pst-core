20734cd213eaa2befe263dfcfd1fc45780d6e9fd
Jonathan Brown <jonathan@resultsathand.com>
2017-10-10 10:54:39 -0400

    Working on changing the default filter wording for pst/core#21

--- application/views/master/master_v_brand.php
+++ application/views/master/master_v_brand.php
@@ -27,6 +27,12 @@ if (isset($keywords) &&	$keywords != "") {
         $meta_keywords = $pageRec["keywords"];
 }
 
+
+if (!defined('SEARCH_PLACEHOLDER_WORDING')) {
+    define('SEARCH_PLACEHOLDER_WORDING', 'Search Parts and Apparel');
+}
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -300,7 +306,7 @@ if (isset($keywords) &&	$keywords != "") {
 			</div>	
 			<div class="searchHolder search-one">
 				<form action="<?php echo base_url(); ?>shopping/productlist" method="post" id="moto_search" class="form_standard">
-					<input id="search" name="search" placeholder="Search Parts and Apparel" class="search-bx" style="float:left;" />
+					<input id="search" name="search" placeholder="<?php echo SEARCH_PLACEHOLDER_WORDING; ?>" class="search-bx" style="float:left;" />
 					<a href="javascript:void(0);" class="goBtn_b" onClick="setSearch($('#search').val());">Go!</a>
 				</form>
 				<div class="clear"></div>
