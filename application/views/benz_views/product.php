c9d06d1a965f97ec93cbb131b3c185fe1766fbf7
mparisi <mgparisicpu@gmail.com>
2017-10-18 23:02:47 -0400

    Quick Cleanup and Refactor of code.

--- application/views/benz_views/product.php
+++ application/views/benz_views/product.php
@@ -124,13 +124,11 @@ $media_url = jsite_url("/media/");
                                 <?php if ($motorcycle['call_on_price'] == '1') { ?>
                                     <p class="cfp">Call For Price</p>
                                 <?php } else {
-                                    if ($motorcycle['sale_price']>0 && $motorcycle['sale_price']!=="0.00") {
-                                        ?>
+                                    if ($motorcycle['sale_price']>0 && $motorcycle['sale_price']!=="0.00") { ?>
                                         <p>Retail Price: &nbsp; <span class="strikethrough">$<?php echo $motorcycle['retail_price']; ?></span></p>
                                         <p>Sale Price: &nbsp;&nbsp;&nbsp;<span class="redtext">$<?php echo $motorcycle['sale_price']; ?></span></p>
-                                        <?php
-                                    } else {
-                                        ?><p>Retail Price: &nbsp; $<?php echo $motorcycle['retail_price']; ?></p><?php
+                                     <?php } else { ?>
+                                        <p>Retail Price: &nbsp; $<?php echo $motorcycle['retail_price']; ?></p><?php
                                     }
                                     if ($motorcycle["destination_charge"]) {
                                         ?><sub>* Plus Applicable destination charge</sub><?php
