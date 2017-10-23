b604b3eeca910813edc48d558665e4be87bb14dd
Jonathan Brown <jonathan@resultsathand.com>
2017-10-22 21:53:38 -0400

    HEre for #6

--- application/views/widgets/product_band_v.php
+++ application/views/widgets/product_band_v.php
@@ -107,7 +107,7 @@ if (!function_exists('tag_creating')) {
 
                         <div class="product_photo" >
                             <?php if ($has_fitment && ((array_key_exists("activeRide", $prod) && $prod["activeRide"]) || (array_key_exists("universal_fitment", $prod) && $prod["universal_fitment"] > 0))): ?>
-                            <div class="product_icon" ><img src="<?php $height = 42; if(@$prod['activeRide']) { echo $fitment_image; $width = round($fitment_width * $height / $fitment_height, 0);  } else if (array_key_exists("universal_fitment", $prod) && $prod["universal_fitment"] > 0) { echo $universal_image; $width = round($universal_width * $height / $universal_height, 0); } ?>" height="<?php echo $height; ?>" width="<?php echo $width; ?>" ></div>
+                            <div class="product_icon" ><img src="<?php $height = 42; if(@$prod['activeRide']) { $alt_tag = "Perfect Fit"; echo $fitment_image; $width = round($fitment_width * $height / $fitment_height, 0);  } else if (array_key_exists("universal_fitment", $prod) && $prod["universal_fitment"] > 0) { $alt_tag = "Universal Fit"; echo $universal_image; $width = round($universal_width * $height / $universal_height, 0); } ?>" alt="<?php echo $alt_tag; ?>" height="<?php echo $height; ?>" width="<?php echo $width; ?>" ></div>
                                 <?php else: ?>
                                     <div class="product_icon" ></div>
                                 <?php endif; ?>
