c5e274c2eaf6e5663375dc91b03fb5141766d2d5
Jonathan Brown <jonathan@resultsathand.com>
2017-10-25 18:10:07 -0400

    Problem with the pricing in the recently viewed stuff

--- application/views/benz_views/product.php
+++ application/views/benz_views/product.php
@@ -92,11 +92,25 @@ $media_url = jsite_url("/media/");
                         </a>
                         <a href="<?php echo base_url(strtolower($recently['type']) . '/' . $title . '/' . $recently['sku']); ?>"><h1 class="head-txt"><?php echo $recently['title']; ?></h1></a>
                         <!--<p><?php echo $recently['title']; ?></p>-->
-                        <?php if ($recently['call_on_price'] == '1') { ?>
+                        <?php if( $recently['call_on_price'] == '1' ) { ?>
                             <p class="cfp">Call For Price</p>
-                        <?php } else { ?>
-                            <span>$<?php echo number_format($recently['sale_price'], 2); ?></span>
-                        <?php } ?>
+                            <?php
+                        } else {
+                            if ($recently['sale_price'] > 0 && $recently['sale_price'] !== "0.00") { ?>
+                                <p>Retail Price: &nbsp; <span
+                                            class="strikethrough">$<?php echo number_format($recently['retail_price'], 2); ?></span>
+                                </p>
+                                <p>Sale Price: &nbsp; &nbsp;<span
+                                            class="redtext">$<?php echo number_format($recently['sale_price'], 2); ?></span></p>
+                            <?php } else { ?>
+                                <p>Retail Price: &nbsp; $<?php echo number_format($recently['retail_price'], 2); ?></p>
+                                <?php
+                            }
+                            if ($recently["destination_charge"]) {
+                                echo "<sub>* Plus Applicable destination charge</sub>";
+                            }
+                        }
+                        ?>
                     </div>
                 <?php } ?>
             </div>		
@@ -250,9 +264,6 @@ $media_url = jsite_url("/media/");
                         <h4 style="margin:0 0 20px">RECENTLY VIEWED</h4>
                     </div>
                     <div class="fltrbx">
-                        <!--
-                        <?php print_r($recentlyMotorcycle); ?>
-                        -->
                         <?php foreach ($recentlyMotorcycle as $recently) { ?>
                             <?php $title = str_replace(' ', '_', trim($recently['title'])); ?>
                             <div class="col-md-12 text-center padg">
