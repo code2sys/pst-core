fd958fd3ab6743bb8ab5a46ec91044e15d6fc600
Jonathan Brown <jonathan@resultsathand.com>
2017-10-25 17:20:15 -0400

    core/issues#39

--- application/views/benz_views/filter-product.php
+++ application/views/benz_views/filter-product.php
@@ -22,11 +22,16 @@ if (@$motorcycles) {
                     <h3><?php echo $motorcycle['title']; ?></h3>
                     <?php if ($motorcycle['call_on_price'] == '1') { ?>
                         <p class="cfp">Call For Price</p>
-                    <?php } else { ?>
-                        <p>Retail Price<?php echo $motorcycle['retail_price']; ?></p><?php
-                        if ($motorcycle['sale_price'] > 0 && $motorcycle['sale_price'] !== "0.00") {?>
-                            <p>Sale Price: &nbsp; &nbsp;&nbsp;$<?php echo $motorcycle['sale_price']; ?></p>
-                        <?php }
+                    <?php } else {
+                        if ($motorcycle['sale_price']>0 && $motorcycle['sale_price']!=="0.00") { ?>
+                        <p>Retail Price: &nbsp; <span class="strikethrough">$<?php echo $motorcycle['retail_price']; ?></span><br>
+                            Sale Price: &nbsp;&nbsp;&nbsp;&nbsp;<span class="redtext">$<?php echo $motorcycle['sale_price']; ?></span></p>
+                        <?php } else { ?>
+                            <p>Retail Price: &nbsp; $<?php echo $motorcycle['retail_price']; ?></p><?php
+                        }
+                        if ($motorcycle["destination_charge"]) {
+                            ?><sub>* Plus Applicable destination charge</sub><?php
+                        }
                     }
                 ?>
                 </div>
