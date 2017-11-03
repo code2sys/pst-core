cb0b1dcda83d1076742199f002ad9bbfe96f4401
Jonathan Brown <jonathan@resultsathand.com>
2017-11-03 09:32:45 -0400

    Debuggin

--- application/models/productuploadermodel.php
+++ application/models/productuploadermodel.php
@@ -620,6 +620,9 @@ class Productuploadermodel extends CI_Model {
 
     // Reference: Controllers/Adminproduct::product_add_save
     protected function sub_apply($row, $distributor_id, $partvariation_id = 0) {
+        print "Call to sub_apply with distributor $distributor_id partvariation $partvariation_id \n";
+        print_r($row);
+
         // you should plow through all of it - name, manufacturer, description, categories... that's what we put into product_add_save...
         $part_name = trim($row["name"]);
         $manufacturer = trim($row["manufacturer"]);
