9d152ea2b9b38edbe9a2c190fa56874657df073c
Jonathan Brown <jonathan@resultsathand.com>
2017-11-21 17:21:48 -0500

    Working at perfect...or good enough...here

--- application/models/crs_m.php
+++ application/models/crs_m.php
@@ -77,6 +77,8 @@ class CRS_M extends Master_M
         curl_setopt($ch, CURLOPT_SSL_VERIFYSTATUS, FALSE);
         $output = curl_exec($ch);
 
+        print_r($output);
+
         //Process CRS into arrays.
         $crsFullData = json_decode($output, true);
         if (array_key_exists("success", $crsFullData)) {
