1814394bc8f5d9ad7258a548bb59d12211462f80
Jonathan Brown <jonathan@resultsathand.com>
2017-11-21 17:33:20 -0500

    Working at perfect...or good enough...here

--- application/models/crs_m.php
+++ application/models/crs_m.php
@@ -74,8 +74,8 @@ class CRS_M extends Master_M
         $ch = curl_init(self::BASE_CRS_URL . $function);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $arguments);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         $output = curl_exec($ch);
         $info = curl_getinfo($ch);
         print_r($info);
