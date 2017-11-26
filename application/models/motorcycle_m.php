ddee67c59008a2c64c8d4034479831df3c6ed12a
Jonathan Brown <jonathan@resultsathand.com>
2017-11-26 17:46:56 -0500

    Trying to pull motorcycles

--- application/models/motorcycle_m.php
+++ application/models/motorcycle_m.php
@@ -249,9 +249,8 @@ class Motorcycle_M extends Master_M {
 
         $this->db->join('motorcycle_type', 'motorcycle.vehicle_type = motorcycle_type.id', 'left');
 
-        $this->db->join(' (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id', 'left');
-
-//        $this->db->join('motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id', 'left');
+        $this->db->join(' (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimageA', 'motorcycleimageA.motorcycle_id = motorcycle.id', 'left');
+        $this->db->join('motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id and motorcycleimage.priority_number = motorcycleimageA.priority_number ', 'left');
         $this->db->select('motorcycle.*, motorcycleimage.image_name, motorcycle_type.name as type, motorcycleimage.external');
         $this->db->group_by('motorcycle.id');
         $this->db->_protect_identifiers=true;
@@ -266,7 +265,8 @@ class Motorcycle_M extends Master_M {
         //$where = array('motorcycle.featured' => '1' );
         $this->db->_protect_identifiers=false;
         $this->db->join('motorcycle_type', 'motorcycle.vehicle_type = motorcycle_type.id', 'left');
-        $this->db->join(' (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id', 'left');
+        $this->db->join(' (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimageA', 'motorcycleimageA.motorcycle_id = motorcycle.id', 'left');
+        $this->db->join('motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id and motorcycleimage.priority_number = motorcycleimageA.priority_number ', 'left');
         $this->db->group_by('motorcycle.id');
         $this->db->limit("3");
         $this->db->order_by("motorcycle.id","DESC");
