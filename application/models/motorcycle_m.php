14fea94deae687523a4ff069ccce367c7ef432e3
Jonathan Brown <jonathan@resultsathand.com>
2017-12-14 13:03:30 -0500

    Here

--- application/models/motorcycle_m.php
+++ application/models/motorcycle_m.php
@@ -255,23 +255,11 @@ class Motorcycle_M extends Master_M {
         return $record;
     }
 
-    public function getFeaturedMonster() {
-        $where = array(
-            'motorcycle.featured' => '1',
-            'motorcycleimage.priority_number' => '0',
-        );
-        $this->db->_protect_identifiers=false;
-
-        $this->db->join('motorcycle_type', 'motorcycle.vehicle_type = motorcycle_type.id', 'left');
-
-        $this->db->join(' (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimageA', 'motorcycleimageA.motorcycle_id = motorcycle.id', 'left');
-        $this->db->join('motorcycleimage', 'motorcycleimage.motorcycle_id = motorcycle.id and motorcycleimage.priority_number = motorcycleimageA.priority_number ', 'left');
-        $this->db->select('motorcycle.*, motorcycleimage.image_name, motorcycle_type.name as type, motorcycleimage.external');
-        $this->db->group_by('motorcycle.id');
-        $this->db->_protect_identifiers=true;
-
-        $records = $this->selectRecords('motorcycle', $where);
-        return $records;
+    public function getFeaturedMonster()
+    {
+        $query = "Select motorcycle.*, motorcycleimage.image_name, motorcycle_type.name as type, motorcycleimage.external from motorcycle join motorcycle_type on motorcycle.vehicle_type = motorcycle_type.id left join (select min(priority_number) as priority_number, motorcycle_id, external from motorcycleimage where disable = 0 group by motorcycle_id) motorcycleimageA on motorcycleimageA.motorcycle_id = motorcycle.id left join motorcycleimage on motorcycleimage.motorcycle_id = motorcycle.id and motorcycleimage.priority_number = motorcycleimageA.priority_number where motorcycle.featured = 1  group by motorcycle.id";
+        $query = $this->db->query($query);
+        return $query->result_array();
     }
 
     public function getReccentlyMotorcycles( $ids ) {
