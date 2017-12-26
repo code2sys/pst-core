62e82ad2cd5989b45bb9e68b8343d92158697cb3
Jonathan Brown <jonathan@resultsathand.com>
2017-12-26 13:21:53 -0500

    Here

--- application/views/admin/motorcycle/quotes_index.php
+++ application/views/admin/motorcycle/quotes_index.php
@@ -40,7 +40,7 @@
         <!-- END VALIDATION ALERT -->
 
 
-        <div class="admin_search_left">
+        <div class="">
             <div class="clear"></div>
             <h1><i class="fa fa-motorcycle"></i> Quote Requests</h1>
         </div>
