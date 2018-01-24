aef509857eea2a3ac87ce423f7f621a7caf55892
Jonathan Brown <jonathan@jlbbackup.local>
2018-01-24 10:18:38 -0500

    What did they do with this auto play thing?

--- application/views/master/footer_v_new.php
+++ application/views/master/footer_v_new.php
@@ -61,8 +61,8 @@ echo $CI->load->view("braintree", array(
  
 		$("#owl-demo").owlCarousel({
 		 	  navigation : true,
-			   slideSpeed : <?php echo defined("HOME_SCREEN_SLIDER_SPEED") ? HOME_SCREEN_SLIDER_SPEED : 300; ?>,
-			   paginationSpeed : <?php echo defined("HOME_SCREEN_PAGINATION_SPEED") ? HOME_SCREEN_PAGINATION_SPEED : 400; ?>,
+			   slideSpeed : <?php echo defined("HOME_SCREEN_SLIDER_SPEED") ? HOME_SCREEN_SLIDER_SPEED : 500; ?>,
+			   paginationSpeed : <?php echo defined("HOME_SCREEN_PAGINATION_SPEED") ? HOME_SCREEN_PAGINATION_SPEED : 500; ?>,
 			   singleItem:true,
 			   autoPlay: <?php echo defined("HOME_SCREEN_AUTO_PLAY_TIMEOUT") ? HOME_SCREEN_AUTO_PLAY_TIMEOUT : 5000; ?>,
 			   autoPlayTimeout:<?php echo defined("HOME_SCREEN_AUTO_PLAY_TIMEOUT") ? HOME_SCREEN_AUTO_PLAY_TIMEOUT : 1000; ?>
