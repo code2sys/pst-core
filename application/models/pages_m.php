ba91e409db5487f4985f592fc3c349479c7c62b2
Jonathan Brown <jonathan@resultsathand.com>
2018-03-12 13:19:16 -0400

    Make it so they can change the time

--- application/models/pages_m.php
+++ application/models/pages_m.php
@@ -267,7 +267,7 @@ class Pages_M extends Master_M
                             $correct_ordinal++;
                             $data['sliderImages'][$correct_ordinal] = $img;
                         }
-                        $data["slider_seconds"] = intVal(1000 * $section["slider_seconds"]);
+                        $data["slider_transition_time"] = intVal(1000 * $section["slider_seconds"]);
                         $widgetBlock .= $this->load->view('widgets/slider_v', $data, TRUE);
                         $widgetBlock .='<br />';
                     }
