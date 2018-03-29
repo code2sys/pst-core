28fcfbb4ffe0708b551a2c60303cfef1cc285d96
Jonathan Brown <jonathan@resultsathand.com>
2018-03-29 18:45:57 -0400

    pst/core#90 working on the banner

--- application/views/store_header_banner.php
+++ application/views/store_header_banner.php
@@ -1,1 +1,10 @@
+<style>
+    .storeHeaderBanner {
+        min-height: 50px;
+        margin-top: 6px;
+        padding-top: 3px;
+        padding-bottom: 3px;
+    }
+
+</style>
 <div class='storeHeaderBanner' style="background-color: <?php echo $store_header_banner_bgcolor; ?>"><div class='container_b'><?php echo $store_header_banner_contents; ?></div></div>
