de3880a191db4b7e464d27e35f7cae116c2e690f
Jonathan Brown <jonathan@resultsathand.com>
2018-03-30 11:46:02 -0400

    pst/core#90 working on the banner

--- application/views/admin/profile_v.php
+++ application/views/admin/profile_v.php
@@ -369,6 +369,7 @@ if (!defined("ENABLE_OEMPARTS_BUTTON")) {
 
 
                                 $("#store_header_banner_bgcolor_div").jqxColorPicker({ width: 200, height: 200 });
+                                $("#store_header_banner_bgcolor_div").jqxColorPicker("setColor", "<?php echo substr($store_header_banner_bgcolor, 1); ?>");
                                 $('#store_header_banner_bgcolor_div').bind('colorchange', function (event)
                                 {
                                     var color = event.args.color;
