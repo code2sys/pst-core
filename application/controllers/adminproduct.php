9d3da261234822a51cc20dfa15e97ef6a3cf6de7
Jonathan Brown <jonathan@resultsathand.com>
2018-04-05 17:31:23 -0400

    Build out for pst/core#93

--- application/controllers/adminproduct.php
+++ application/controllers/adminproduct.php
@@ -504,45 +504,33 @@ class Adminproduct extends Admin {
     }
 
     public function ajax_product_question_answer_add($part_id) {
-        error_log("a");
         $part = $this->admin_m->getAdminProduct($part_id);
-        error_log("b");
 
         if ($part["mx"] != 0) {
             $this->Statusmodel->setError("Sorry, that is not an editable part number.");
         } else {
-            error_log("c");
             $answer = array_key_exists("answer", $_REQUEST) ? $_REQUEST["answer"] : "";
             $question = array_key_exists("question", $_REQUEST) ? $_REQUEST["question"] : "";
             $partnumber_id = array_key_exists("partnumber_id", $_REQUEST) ? $_REQUEST["partnumber_id"] : 0;
-            error_log("d");
 
             $partquestion_id = $partnumberpartquestion_id = 0;
-            error_log("e");
 
             if ($question == "") {
-                error_log("e1");
                 $this->Statusmodel->setError("Sorry, no question text received.");
             } else if ($answer == "") {
-                error_log("e2");
                 $this->Statusmodel->setError("Sorry, no answer text received.");
             } else if ($partnumber_id == 0) {
-                error_log("e3");
                 $this->Statusmodel->setError("Sorry, no distributor part number received.");
             } else if ($this->Portalmodel->addPartProductAnswer($part_id, $question, $answer, $partnumber_id, $partquestion_id, $partnumberpartquestion_id)) {
-                error_log("e4");
                 // we need to set the data...
 
                 $this->Statusmodel->setData("partquestion_id", $partquestion_id);
                 $this->Statusmodel->setData("partnumberpartquestion_id", $partnumberpartquestion_id);
                 $this->Statusmodel->setSuccess("Answer added successfully.");
             } else {
-                error_log("e5");
                 $this->Statusmodel->setError("Sorry, that question is not a category filter question.");
             }
-            error_log("f");
         }
-        error_log("g");
 
         $this->Statusmodel->outputStatus();
     }
