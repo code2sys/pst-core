8d2f8041b30c9c4b1f3332913dc41a104ad8dba6
Jonathan Brown <jonathan@resultsathand.com>
2018-04-05 17:11:51 -0400

    Build out for pst/core#93

--- application/models/portalmodel.php
+++ application/models/portalmodel.php
@@ -68,7 +68,7 @@ class Portalmodel extends Master_M {
             $partquestion_id = $this->db->insert_id();
 
             // we almost certainly have to add it into product question
-            $this->db->query("Insert into productquestion (part_id, question) values (?, ?) on key duplicate update productquestion_id = last_insert_id(productquestion_id)", array($part_id, $question));
+            $this->db->query("Insert into productquestion (part_id, question) values (?, ?) on duplicate key update productquestion_id = last_insert_id(productquestion_id)", array($part_id, $question));
             error_log("Insert into product question");
             $productquestion_id = $this->db->insert_id();
             error_log("Retrieved productquestion $productquestion_id");
