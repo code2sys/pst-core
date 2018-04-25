c1a688bb55e7a526b64e2c56ab38c5ba0dfe81c2
Jonathan Brown <jonathan@resultsathand.com>
2018-04-25 09:39:28 -0400

    Working on cleaning up modesto pst/core#103

--- application/models/crscron_m.php
+++ application/models/crscron_m.php
@@ -71,7 +71,7 @@ class CRSCron_M extends Master_M
             foreach ($attributes as $a) {
                 $motorcyclespecgroup_id = $this->_getAttributeGroup($motorcycle_id, $a["attributegroup_name"], $a["attributegroup_number"]);
 
-                $this->db->query("Insert into motorcyclespec (version_number, value, feature_name, attribute_name, type, external_package_id, motorcycle_id, final_value, source, crs_attribute_id, motorcyclespecgroup_id, ordinal) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on duplicate key update value = If(source = 'PST', values(value), value), final_value = If(source = 'PST' AND override = 0, values(final_value), final_value), motorcyclespecgroup_id = values(motorcyclespecgroup_id), motorcyclespec_id = last_insert_id(motorcyclespec)", array(
+                $this->db->query("Insert into motorcyclespec (version_number, value, feature_name, attribute_name, type, external_package_id, motorcycle_id, final_value, source, crs_attribute_id, motorcyclespecgroup_id, ordinal) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on duplicate key update value = If(source = 'PST', values(value), value), final_value = If(source = 'PST' AND override = 0, values(final_value), final_value), motorcyclespecgroup_id = values(motorcyclespecgroup_id), motorcyclespec_id = last_insert_id(motorcyclespec_id)", array(
                     $a["version_number"],
                     $a["text_value"],
                     $a["feature_name"],
