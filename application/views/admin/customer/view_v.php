9f9fc5f179d376852bf3a393a133e146fcec05a3
Jonathan Brown <jonathan@resultsathand.com>
2018-05-03 19:08:23 -0400

    WOP pst/core#104

--- application/views/admin/customer/view_v.php
+++ application/views/admin/customer/view_v.php
@@ -206,7 +206,7 @@
 				<?php if (ENABLE_CUSTOMER_PRICING) {
 					$CI =& get_instance();
 					?>
-					<div style="width: 45%">
+					<div style="width: 45%; float: left;">
 					<?php echo $CI->load->view("admin/customer/backbone_customerpricing_widget", array("user_id" => $user_id), true); ?>
 					</div>
 					<?php
