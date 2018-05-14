091dda72c51d6287e2a06113d836823779142144
Jonathan Brown <jonathan@resultsathand.com>
2018-05-14 11:25:19 -0400

    Change this to use the footer template

--- application/views/mobile_navigation_fragment.php
+++ application/views/mobile_navigation_fragment.php
@@ -20,20 +20,20 @@ if (!isset($active_primary_navigation)) {
 //}
 
 
-$template = mustache_tmpl_open("mobile_navigation_fragment.html");
-mustache_tmpl_set($template, "s_baseURL", $s_baseURL);
+$mobile_navigation_fragment_template = mustache_tmpl_open("mobile_navigation_fragment.html");
+mustache_tmpl_set($mobile_navigation_fragment_template, "s_baseURL", $s_baseURL);
 //if ($partsfinder_link != "") {
-//    mustache_tmpl_set($template, "partsfinder_link", $partsfinder_link);
+//    mustache_tmpl_set($mobile_navigation_fragment_template, "partsfinder_link", $partsfinder_link);
 //}
 
 // We have to dump out some more navigation items here in a list...
 if (!isset($SIMPLIFIED_NAV_WITHIN_MAJOR_UNITS) || !$SIMPLIFIED_NAV_WITHIN_MAJOR_UNITS) {
-    mustache_tmpl_set($template, "active_primary_navigation", $active_primary_navigation);
+    mustache_tmpl_set($mobile_navigation_fragment_template, "active_primary_navigation", $active_primary_navigation);
 }
 
 
 if (isset($GLOBAL_MOBILE_NAV_FRAG_STRING) && $GLOBAL_MOBILE_NAV_FRAG_STRING) {
-    $GLOBAL_MOBILE_NAV_FRAG =  mustache_tmpl_parse($template);
+    $GLOBAL_MOBILE_NAV_FRAG =  mustache_tmpl_parse($mobile_navigation_fragment_template);
 } else {
-    echo mustache_tmpl_parse($template);
+    echo mustache_tmpl_parse($mobile_navigation_fragment_template);
 }
