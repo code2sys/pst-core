f54a41de4cfb138ded968f155da90e59ea1038f2
Jonathan Brown <jonathan@resultsathand.com>
2018-05-31 18:40:23 -0400

    pst/core#112 Basic structure for the CodeIgniter RESTful web service.

--- application/helpers/jonathan_helper.php
+++ application/helpers/jonathan_helper.php
@@ -222,3 +222,36 @@ function jserve_file($source_file_path, $filename, $mime_type) {
     
  }
 
+
+
+// http://stackoverflow.com/questions/2916232/call-to-undefined-function-apache-request-headers
+if( !function_exists('apache_request_headers') ) {
+///
+    function apache_request_headers() {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+        foreach($_SERVER as $key => $val) {
+            if( preg_match($rx_http, $key) ) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                // do some nasty string manipulations to restore the original letter case
+                // this should work in most cases
+                $rx_matches = explode('_', $arh_key);
+                if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                    foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
+                    $arh_key = implode('-', $rx_matches);
+                }
+                $arh[$arh_key] = $val;
+            }
+        }
+        return( $arh );
+    }
+///
+}
+///
+
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        return apache_Request_headers();
+    }
+}
