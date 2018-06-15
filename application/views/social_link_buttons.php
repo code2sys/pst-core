26e04b122255c2d6543637aa67ea6d426f00e51b
Jonathan Brown <jonathan@resultsathand.com>
2018-06-15 14:49:23 -0400

    Factoring out templates for Kyle

--- application/views/social_link_buttons.php
+++ application/views/social_link_buttons.php
@@ -9,7 +9,7 @@ $CI =& get_instance();
 $CI->load->helper("mustache_helper");
 $template = mustache_tmpl_open("social_link_buttons.html");
 
-foreach (array() as $link) {
+foreach (array("sm_fblink", "sm_twlink", "sm_ytlink", "sm_gplink", "sm_insta", "sm_amazon", "sm_ebay", "sm_snapchat") as $link) {
     if (array_key_exists($link, $SMSettings)) {
         mustache_tmpl_set($template, $link, $SMSettings[$link]);
     }
