41b6fd9158776f2744146d201f54cef0dc0c9cd8
Jonathan Brown <jonathan@resultsathand.com>
2018-06-16 14:59:41 -0400

    Make these hours more available

--- application/views/info/store_hours.php
+++ application/views/info/store_hours.php
@@ -1,4 +1,8 @@
 <?php
 
-
+$CI =& get_instance();
+$CI->load->helper("mustache_helper");
+$store_hours_template = mustache_tmpl_open("info/store_hours.html");
+jtemplate_add_store_hours($store_hours_template, $store_name);
+echo mustache_tmpl_parse($store_hours_template);
 
