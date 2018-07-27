27f6c95391236a9abcc549ea37852802c6f40cd7
Jonathan Brown <jonathan@resultsathand.com>
2018-07-27 11:46:46 -0400

    Working on pst/core#120 Changing the footer if you do not have the Braintree

--- application/views/braintree.php
+++ application/views/braintree.php
@@ -1,15 +1,13 @@
 <?php
 
-if (isset($store_name) && array_key_exists("merchant_type", $store_name) && $store_name["merchant_type"] == "Braintree") {
+$CI =& get_instance();
+$CI->load->helper("mustache_helper");
+$template = mustache_tmpl_open("braintree.html");
+$new_assets_url = jsite_url("/qatesting/newassets/");
+$new_assets_url1 = jsite_url("/qatesting/benz_assets/");
 
-    $CI =& get_instance();
-    $CI->load->helper("mustache_helper");
-    $template = mustache_tmpl_open("braintree.html");
-    $new_assets_url = jsite_url("/qatesting/newassets/");
-    $new_assets_url1 = jsite_url("/qatesting/benz_assets/");
+mustache_tmpl_set($template, "new_assets_url", $new_assets_url);
+mustache_tmpl_set($template, "merchant_id", $store_name['merchant_id']);
+mustache_tmpl_set($template, "braintree", $store_name["merchant_type"] == "Braintree");
 
-    mustache_tmpl_set($template, "new_assets_url", $new_assets_url);
-    mustache_tmpl_set($template, "merchant_id", $store_name['merchant_id']);
-
-    echo mustache_tmpl_parse($template);
-}
+echo mustache_tmpl_parse($template);
