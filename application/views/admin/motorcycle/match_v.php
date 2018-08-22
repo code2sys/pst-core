ce83095854df0ca5768df8fb7f06ddeca3ea4611
Jonathan Brown <jonathan@resultsathand.com>
2018-08-22 11:03:36 -0400

    Echo the trim

--- application/views/admin/motorcycle/match_v.php
+++ application/views/admin/motorcycle/match_v.php
@@ -1,3 +1,27 @@
+<?php
+$CI =& get_instance();
+$CI->load->model("CRS_m");
+
+?>
+<!-- Gritter -->
+<link rel="stylesheet"
+      href="/assets/Gritter/css/jquery.gritter.css" />
+<!--<link rel="stylesheet" href="/assets/newjs/jquery-ui.structure.min.css" />-->
+<link rel="stylesheet" href="/assets/newjs/jquery-ui.min.css" />
+
+<script type="text/javascript"
+        src="/assets/Gritter/js/jquery.gritter.min.js"></script>
+
+<script type="application/javascript" src="/assets/underscore/underscore-min.js" ></script>
+<script type="application/javascript" src="/assets/backbone/backbone-min.js" ></script>
+<script type="application/javascript" src="/assets/dropzone/dropzone.js" ></script>
+<script type="application/javascript" src="/assets/newjs/jquery-ui.min.js" ></script>
+<script type="text/template" id="ExistingMatchView">
+
+</script>
+<script type="text/template" id="AvailableTrimView">
+
+</script>
 <div class="content_wrap">
     <div class="content">
 
@@ -15,8 +39,30 @@ echo $CI->load->view("admin/motorcycle/moto_head", array(
     "stock_status" => @$product["stock_status"]
 ), true);
 
-$suppress = $id > 0 && $product["crs_trim_id"] > 0;
-
 ?>
+<div class="tab_content">
+    <?php if ($product["crs_trim_id"] > 0): ?>
+    <div class="existing_trim_holder">
+        <?php print_r($CI->CRS_m->getTrim($product["crs_trim_id"])); ?>
+    </div>
+    <?php endif; ?>
+
+
+</div>
+
+
+<script type="application/javascript">
+$(document).on("ready", function() {
+
+    <?php if ($product["crs_trim_id"] > 0): ?>
+
+    <?php endif; ?>
+
+
+});
+
+
+</script>
+
     </div>
 </div>
