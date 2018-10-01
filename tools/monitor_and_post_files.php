9f312be704c71e0ad4f14136852e54d8af081ad0
Jonathan Brown <jonathan@resultsathand.com>
2018-10-01 17:34:15 -0400

    Monitoring file for pst/core#133

--- tools/monitor_and_post_files.php
+++ tools/monitor_and_post_files.php
@@ -79,14 +79,38 @@ if ($in_dir = opendir($incoming_directory)) {
         if ($entry != "." && $entry != "..") {
             // attempt to process the file..
             // generate the error output
+            $output_file = $outgoing_directory . "/" . time() . "_" . $entry . ".output.txt";
 
             // Check that it is a CSV file
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            if ("text/plain" != finfo_file($finfo, $incoming_directory . "/" . $entry)) {
+                file_put_contents($output_file, "Invalid file format received; please upload a CSV file.");
+                continue;
+            }
 
             // Move it to temp
             $target_file = tempnam($tmp_directory, "monitored_") . $entry;
             system("mv $incoming_directory/$entry $target_file");
 
             // Now, attempt to post and write to the output file
+            // https://blog.derakkilgo.com/2009/06/07/send-a-file-via-post-with-curl-and-php/
+            /* curl will accept an array here too.
+             * Many examples I found showed a url-encoded string instead.
+             * Take note that the 'key' in the array will be the key that shows up in the
+             * $_FILES array of the accept script. and the at sign '@' is required before the
+             * file name.
+             */
+            $file_name_with_full_path = realpath($incoming_directory . "/" . $entry);
+            $post = array('upload'=>'@'.$file_name_with_full_path);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$post_url);
+            curl_setopt($ch, CURLOPT_POST,1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+            $result=curl_exec ($ch);
+            curl_close ($ch);
+            file_put_contents($output_file, $result);
 
             // Remove the file, if the flag is set.
             if ($delete_file) {
