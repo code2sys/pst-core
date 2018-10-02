c7fd85804471fa0f5f12acc9e124fa51071fc786
Jonathan Brown <jonathan@resultsathand.com>
2018-10-02 17:15:21 -0400

    pst/core#133

--- application/controllers/abstractadmin/motorcycleadmin.php
+++ application/controllers/abstractadmin/motorcycleadmin.php
@@ -67,8 +67,9 @@ abstract class Motorcycleadmin extends Firstadmin
         global $PSTAPI;
         initializePSTAPI();
         foreach ($this->_dealerTrackConfigs() as $key => $default) {
-            error_log("Setting key: $key");
-            $PSTAPI->config()->setKeyValue($key, array_key_exists($key, $_REQUEST) ? $_REQUEST[$key] : $default);
+            $value = array_key_exists($key, $_REQUEST) ? $_REQUEST[$key] : $default;
+            error_log("Setting key: $key to $value");
+            $PSTAPI->config()->setKeyValue($key, $value);
         }
 
         // now, set some sort of success flag...
