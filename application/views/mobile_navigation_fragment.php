fd3194bd2673bc77dccde99cc1218ec18846978b
Jonathan Brown <jonathan@resultsathand.com>
2018-10-22 10:31:09 -0400

    Change the way that the front end buttons show regardless of featured and also add New Units, Pre-Owned, and Service to the mobile menu. pst/Jons-projects#15

--- application/views/mobile_navigation_fragment.php
+++ application/views/mobile_navigation_fragment.php
@@ -18,6 +18,31 @@ if (!isset($active_primary_navigation)) {
 //    $store_name = $CI->admin_m->getAdminShippingProfile();
 //    $partsfinder_link = $store_name["partsfinder_link"];
 //}
+$extra_navigation = array();
+// You have to explicitly turn NEW ON.
+if (defined("MOTORCYCLE_SHOP_NEW") && MOTORCYCLE_SHOP_NEW) {
+    $extra_navigation[] = array(
+        "url" => "/Motorcycle_List?fltr=new&search_keywords=",
+        "mobile_label" => "New Units"
+    );
+}
+
+// This was how it was done in motorcycles.php. You have to explicitly turn used OFF.
+if (!defined("MOTORCYCLE_SHOP_USED") || MOTORCYCLE_SHOP_USED) {
+    $extra_navigation[] = array(
+        "url" => "/Motorcycle_List?fltr=new&search_keywords=",
+        "mobile_label" => "Pre-Owned Units"
+    );
+}
+
+// OK, we only include the service if one of two things: You included the other two, OR you defined a service page URL constant.
+if (count($extra_navigation) > 0 || defined('MOTORCYCLE_SERVICE_LINK_URL')) {
+    $extra_navigation[] = array(
+        "url" => defined('MOTORCYCLE_SERVICE_LINK_URL') ? MOTORCYCLE_SERVICE_LINK_URL : "/pages/index/servicerequest",
+        "mobile_label" => "Service",
+    );
+}
+
 
 
 $mobile_navigation_fragment_template = mustache_tmpl_open("mobile_navigation_fragment.html");
