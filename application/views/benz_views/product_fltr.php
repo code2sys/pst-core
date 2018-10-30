7d61edf5856d8e7d17bea4a6977b23d61c00eae6
Robert Garrett <robert.garrett0618@gmail.com>
2018-10-30 14:05:49 -0400

    working on seo

--- application/views/benz_views/product_fltr.php
+++ application/views/benz_views/product_fltr.php
@@ -60,7 +60,7 @@ if (array_key_exists("brands", $_REQUEST) && array_key_exists("brands", $_GET)) 
     $tmps = explode('$', $tmps[1]);
 
     foreach( $tmps as $tmp ) {
-        $paramBrands .= "_".str_replace(" ", "-", $tmp);
+        $paramBrands .= "_".str_replace(array('%20', ' '), array('-', '-'), $tmp);
     }
 }
 
@@ -71,7 +71,7 @@ if (array_key_exists("vehicles", $_REQUEST) && array_key_exists("vehicles", $_GE
     $tmps = explode('$', $tmps[1]);
 
     foreach( $tmps as $tmp ) {
-        $paramVehicles .= "_".str_replace(" ", "-", $tmp);
+        $paramVehicles .= "_".str_replace(array('%20', ' '), array('-', '-'), $tmp);
     }
 }
 if (array_key_exists("years", $_REQUEST) && array_key_exists("years", $_GET)) {
