69175011e3ed28ce80f44f515ec6de4cb730a0ce
Robert Garrett <robert.garrett0618@gmail.com>
2018-10-31 16:53:41 -0400

    fix pagination issue

--- application/views/benz_views/product_pagination.php
+++ application/views/benz_views/product_pagination.php
@@ -9,9 +9,7 @@
 $CI =& get_instance();
 $CI->load->helper("mustache_helper");
 $template = mustache_tmpl_open("benz_views/product_pagination.html");
-echo "<pre>";
-echo $pages."###".$cpage;
-echo "</pre>";
+
 mustache_tmpl_set($template, "pages", $pages);
 mustache_tmpl_set($template, "page", $cpage);
 mustache_tmpl_set($template, "MorePagesThan1", $pages > 1);
@@ -19,8 +17,8 @@ mustache_tmpl_set($template, "MorePageThan1", $cpage > 1);
 mustache_tmpl_set($template, "MorePageThan2", $cpage > 2);
 mustache_tmpl_set($template, "MorePageThan3", $cpage > 3);
 mustache_tmpl_set($template, "MorePagesThanPage", $cpage < $pages);
-mustache_tmpl_set($template, "MorePagesThanPage1", ($page < ($pages-1)));
-mustache_tmpl_set($template, "MorePagesThanPage2", ($page < ($pages-2)));
+mustache_tmpl_set($template, "MorePagesThanPage1", ($cpage < ($pages-1)));
+mustache_tmpl_set($template, "MorePagesThanPage2", ($cpage < ($pages-2)));
 mustache_tmpl_set($template, "minpage1", $cpage-1);
 mustache_tmpl_set($template, "minpage2", $cpage-2);
 mustache_tmpl_set($template, "plupage1", $cpage+1);
