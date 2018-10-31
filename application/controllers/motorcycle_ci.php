094f71f60530f59f7b428d06843fa68573811f28
Robert Garrett <robert.garrett0618@gmail.com>
2018-10-30 22:40:52 -0400

    fix pagination issue

--- application/controllers/motorcycle_ci.php
+++ application/controllers/motorcycle_ci.php
@@ -344,9 +344,9 @@ class Motorcycle_CI extends Welcome {
         $curPage = intVal($this->input->post("page") ? $this->input->post("page") : 0);
         $offset = ($curPage * $_SESSION["bikeControlShow"]);
 
-        $filter = $this->motorcycle_m->assembleFilterFromRequest();
+        $filter = $this->motorcycle_m->assembleFilterFromRequest(true);
 echo "<pre>";
-print_r($filter);exit;
+print_r($curPage);
         unset($filter['page']);
         // JLB 06-04-17
         // Why was there a separate one for getFilterMotorcycles?? As far as I can tell, it was to separate off the limit vs. offset.
