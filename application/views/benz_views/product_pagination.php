d3b80f7172fb0114992b30a56847d5d015602a1f
Robert Garrett <robert.garrett0618@gmail.com>
2018-10-31 14:24:55 -0400

    fix pagination issue

--- application/views/benz_views/product_pagination.php
+++ application/views/benz_views/product_pagination.php
@@ -10,6 +10,10 @@ $CI =& get_instance();
 $CI->load->helper("mustache_helper");
 $template = mustache_tmpl_open("benz_views/product_pagination.html");
 
+echo "<pre>";
+echo $cpage;
+echo "</pre>";
+
 mustache_tmpl_set($template, "MoreThan1", $pages > 1);
 mustache_tmpl_set($template, "MoreThan2", $pages > 2);
 mustache_tmpl_set($template, "MoreThan3", $pages > 3);
