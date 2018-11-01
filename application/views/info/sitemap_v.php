7dc3728b804059be131ffb6e9967eb0fa3a7c366
Robert Garrett <robert.garrett0618@gmail.com>
2018-10-31 22:56:46 -0400

    Working on Site Map

--- application/views/info/sitemap_v.php
+++ application/views/info/sitemap_v.php
@@ -5,17 +5,19 @@
 ?>
  <h1>Site Map</h1>
 
- <div class="row no-gutters">
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">1</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">2</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">3</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">4</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">5</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">6</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">7</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">8</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">9</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">10</div>
-	 <div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">11</div>
- </div>
+<div class="container">
+	<div class="row no-gutters">
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">1</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">2</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">3</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">4</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">5</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">6</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">7</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">8</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">9</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">10</div>
+		<div class="col-12 col-sm-6 col-md-3" style="min-height: 100px; border: 1px solid red;">11</div>
+	</div>
+</div>
 			
