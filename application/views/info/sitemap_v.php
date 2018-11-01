8c91c4e889fa85ceeae6032d4b35cbb9453ac025
Robert Garrett <robert.garrett0618@gmail.com>
2018-11-01 06:56:59 -0400

    Working on Site Map

--- application/views/info/sitemap_v.php
+++ application/views/info/sitemap_v.php
@@ -7,6 +7,7 @@
  * Time: 20:00 PM
  */
 
+$media_url = jsite_url("/media/");
 
 $CI =& get_instance();
 $CI->load->helper("mustache_helper");
@@ -16,6 +17,30 @@ echo "<pre>";
 print_r($motorcycles[0]);
 echo "</pre>";
 
-mustache_tmpl_set($template, "motorcycles", $motorcycles);
+foreach ($motorcycles as $motorcycle) {
+
+    // What is the default...
+    $motorcycle_image = $motorcycle['image_name'];
+    if ($motorcycle['external'] == 0) {
+        $motorcycle_image = $media_url . $motorcycle_image;
+    }
+
+    if ($motorcycle_image == "" || is_null($motorcycle_image) || $motorcycle_image == $media_url) {
+        $motorcycle_image = "/assets/image_unavailable.png";
+    }
+
+    mustache_tmpl_iterate($template, "motorcycles");
+
+
+    mustache_tmpl_set($template, "motorcycles", array(
+        "url" => base_url(strtolower($motorcycle['type']) . '/' . $motorcycle['url_title'] . '/' . $motorcycle['sku']),
+        "motorcycle_url_title" => $motorcycle["url_title"],
+        "motorcycle_sku" => $motorcycle["sku"],
+        "motorcycle_image" => $motorcycle_image,
+        "motorcycle_title" => $motorcycle["title"],
+        "motorcycle_description" => $motorcycle["description"],
+        "condition" => $motorcycle['condition'] == '1' ? 'New' : 'Pre-Owned'
+    ));
+}
 
 print mustache_tmpl_parse($template);
