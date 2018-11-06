8b18676f92f2fabb468b2a4209d1e2df6d173b5c
Jonathan Brown <jonathan@resultsathand.com>
2018-11-06 12:14:23 -0500

    Send them on home if they bounce to the wrong URL.

--- application/controllers/shopping.php
+++ application/controllers/shopping.php
@@ -650,9 +650,15 @@ class Shopping extends Master_Controller {
         $metaTag = '';
         $record = $this->parts_m->getBrandBySlug($brand);
         if (is_null($brand) || empty($record)) {
-            // Just go on home.
-            header("Location: " . site_url(""));
-            exit();
+            // I suppose this could be a sizing chart because this is STUPID.
+            $brand_sizing_chart = $this->parts_m->getBrandBySizeChart( $brand);
+            if (!is_null($brand_sizing_chart) && !empty($brand_sizing_chart) && FALSE != $brand_sizing_chart) {
+                $this->size_chart($brand);
+            } else {
+                // Just go on home.
+                header("Location: " . site_url(""));
+                exit();
+            }
         } else {
 			unset($_SESSION['search']);
 			$_SESSION['search']['brand'] = array('id' => $record['brand_id'], 'name' => $record['name']);
