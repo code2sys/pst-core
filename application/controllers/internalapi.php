20eb148453ac79c768163d7cb0c357c7220e438b
Jonathan Brown <jonathan@orbro.com>
2018-11-17 17:57:00 -0500

    Feature/20181113.001.rg

--- application/controllers/internalapi.php
+++ application/controllers/internalapi.php
@@ -16,7 +16,8 @@ class Internalapi extends CI_Controller {
         global $Permitted_InternalAPI_IPs;
         if (!isset($Permitted_InternalAPI_IPs)) {
             $Permitted_InternalAPI_IPs = array(
-                "10.132.45.182"
+                "159.203.108.87",
+                "23.27.206.84"
             );
         }
         if (!in_array($_SERVER["REMOTE_ADDR"], $Permitted_InternalAPI_IPs)) {
@@ -26,7 +27,6 @@ class Internalapi extends CI_Controller {
     }
 
     public function dealertrack() {
-
 
         $file = $_FILES["upload"];
 
@@ -87,6 +87,42 @@ class Internalapi extends CI_Controller {
             $PSTAPI->dealerTrackFeedLog()->end($log_id);
             $this->load->model("CRSCron_M");
             $this->CRSCron_M->removeExtraCRSBikes();
+
+        } else {
+            print "No file received.\n";
+        }
+    }
+
+    public function dealermade() {
+        $file = $_FILES["upload"];
+
+        if ($file["size"] > 0) {
+            ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+
+            global $PSTAPI;
+            initializePSTAPI();
+            
+            $image_count = 0;
+
+            // Start importing images
+            $contents = file_get_contents($file["tmp_name"]);
+            $log_id = $PSTAPI->externalMajorUnitImage()->begin($contents);
+
+            // Get the header
+            $handle = fopen($file["tmp_name"], "r");
+            $header = fgetcsv($handle);
+
+            while (FALSE !== ($row = fgetcsv($handle))) {
+                $data = array();
+                for ($i = 0; $i < count($header); $i++) {
+                    $data[strtolower(str_replace(' ', '_', trim($header[$i])))] = $row[$i];
+                }
+
+                $image_count += $PSTAPI->externalMajorUnitImage()->processRow($data);
+            }
+
+            // end importing images
+            $PSTAPI->externalMajorUnitImage()->end($log_id, $image_count);
 
         } else {
             print "No file received.\n";
