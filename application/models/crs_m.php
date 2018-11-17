41802c5edeb0f90994cff74bac12970d30151599
Jonathan Brown <jonathan@resultsathand.com>
2018-11-17 08:13:58 -0500

    Here

--- application/models/crs_m.php
+++ application/models/crs_m.php
@@ -223,7 +223,7 @@ class CRS_M extends Master_M
     }
 
     public function getModels($args = array()) {
-        return $this->postRequest("getMake", $args, "records");
+        return $this->postRequest("getModels", $args, "records");
     }
 
     public function getMakesByMachineType($machine_type) {
