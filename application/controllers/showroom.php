7e312989079c16b94e1e16b36bd4f843b85405dd
Jonathan Brown <jonathan@resultsathand.com>
2018-11-17 09:02:58 -0500

    Here

--- application/controllers/showroom.php
+++ application/controllers/showroom.php
@@ -28,30 +28,88 @@ class Showroom extends Pages {
     public function index() {
         global $PSTAPI;
         $pages = $PSTAPI->pages()->fetch(array(
-            "page_class" => $this->Showcasemodel->_default_main_title()
+            "page_class" => $this->Showcasemodel->_default_main_title(),
+            "active" => 1
         ));
+        $this->_sub_sub_findPage($pages, "");
+    }
 
+    protected function _sub_sub_findPage($pages, $redirect_url) {
         if (count($pages) > 0) {
             parent::index($pages[0]->get("tag"));
         } else {
-            header("Location: " . base_url(""));
+            header("Location: " . base_url($redirect_url));
+            exit();
+        }
+    }
+
+    protected function _sub_FetchByURLTitle($factory, $url_title) {
+        global $PSTAPI;
+        $matches = $PSTAPI->$factory()->fetch(array(
+            "url_title" => $url_title,
+            "deleted" => 0
+        ));
+
+        return count($matches) > 0 ? $matches[0] : false;
+    }
+
+    protected function _subFindPage($factory, $url_title, $fallback_url) {
+        global $PSTAPI;
+        $match = $this->_sub_FetchByURLTitle($factory, $url_title);
+
+        if (FALSE === $match) {
+            header("Location: " . base_url($fallback_url));
+            exit();
+        } else {
+            $page_id = $match->get("page_id");
+
+            if ($page_id > 0) {
+                $this->_sub_sub_findPage(array($PSTAPI->pages()->get($page_id)), $fallback_url);
+            } else {
+                header("Location: " . base_url($fallback_url));
+                exit();
+            }
         }
     }
 
     public function make($make_name) {
+        $this->_subFindPage("showcasemake", $make_name, "Factory_Showroom");
+    }
 
+    protected function _sub_RedirectIfOneChild($factory, $url_title, $id_column, $child_factory, $prefix_segment = "") {
+        global $PSTAPI;
+        $match = $this->_sub_FetchByURLTitle($factory, $url_title);
+        if (FALSE !== $match) {
+            // OK, so what do we have?
+            $child_matches = $PSTAPI->$child_factory->fetch(array(
+                $id_column => $match->id(),
+                "deleted" => 0
+            ));
+
+            if (count($child_matches) == 1) {
+                // redirect it!
+                header("Location: " . site_url($prefix_segment . "/" . $child_matches[0]->get("url_title")));
+                exit();
+            }
+        }
     }
 
     public function machinetype($make_name, $machine_type_name) {
-
+        // Check for children, and intercept if required...
+        $base_url = "Factory_Showroom/$make_name";
+        $this->_sub_RedirectIfOneChild("showcasemachinetype", $machine_type_name, "showcasemachinetype_id", "showcasemachinetype", $base_url);
+        $this->_subFindPage("showcasemachinetype", $machine_type_name, $base_url);
     }
 
     public function model($make_name, $machine_type_name, $model_name) {
-
+        // Check for children, and intercept if required
+        $base_url = "Factory_Showroom/$make_name/$machine_type_name";
+        $this->_sub_RedirectIfOneChild("showcasemodel", $machine_type_name, "showcasemodel_id", "showcasetrim", $base_url);
+        $this->_subFindPage("showcasemodel", $model_name, $base_url);
     }
 
     public function trim($make_name, $machine_type_name, $model_name, $trim_name) {
-
+        $this->_subFindPage("showcasetrim", $trim_name, "Factory_Showroom/$make_name/$machine_type_name/$model_name");
     }
 
 }
