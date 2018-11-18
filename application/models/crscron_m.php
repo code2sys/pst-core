82d81d0599c70d21183c288530bbc40bf2bfe2c7
Jonathan Brown <jonathan@resultsathand.com>
2018-11-17 20:54:37 -0500

    Hide Not Available for pst/Jons-projects#48

--- application/models/crscron_m.php
+++ application/models/crscron_m.php
@@ -101,6 +101,8 @@ class CRSCron_M extends Master_M
             }
         }
 
+        $PSTAPI->motorcyclespec()->suppressAllNotAvailable();
+
         // Now, we need to get the photos...
         $query = $this->db->query("Select motorcycle.id as motorcycle_id, crs_trim_id, IfNull(max(motorcycleimage.version_number), 0) as version_number, IfNull(max(motorcycleimage.priority_number), 0) as ordinal from motorcycle left join (select * from motorcycleimage where crs_thumbnail = 0) motorcycleimage on motorcycle.id = motorcycleimage.motorcycle_id where crs_trim_id > 0 $where group by motorcycle.id");
 
