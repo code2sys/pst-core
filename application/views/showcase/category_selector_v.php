0b0108c7dc04610ffdb193ea85ce9bc8711acba9
Jonathan Brown <jonathan@resultsathand.com>
2018-11-23 12:26:38 -0500

    Factor breadrumbs

--- application/views/showcase/category_selector_v.php
+++ application/views/showcase/category_selector_v.php
@@ -118,217 +118,163 @@ if ($display_models) {
     $display_trims = true;
 }
 
-?>
 
-<!-- CONTENT WRAP =========================================================================-->
-<div class="content_wrap inventory-content-wrap">
+$CI =& get_instance();
+$CI->load->helper("mustache_helper");
+$category_selector_v = mustache_tmpl_open("showcase/category_selector_v.html");
+
+mustache_tmpl_set($category_selector_v, "breadcrumbs", $CI->load->view("showcase/breadcrumbs", array(
+    "title" => $title,
+    "full_url" => $full_url
+), true));
 
 
+if (isset($widgetBlock) && $widgetBlock != "") {
+    mustache_tmpl_set($category_selector_v, "widgetBlock", $widgetBlock);
+}
 
-    <!-- MAIN CONTENT -->
-    <div class="main_content fl-wdh full_info_content" style="float: none !important; width: 100% !important;">
+echo mustache_tmpl_parse($category_selector_v);
 
-        <?php
-        $CI =& get_instance();
-        echo $CI->load->view("showcase/breadcrumbs", array(
+
+$grid_widgets = array();
+function prepare_widget_group($title, $source_array, &$grid_widgets, $use_display_title = false, $sorted_already = false, $subtitle = "") {
+    if (!$sorted_already) {
+        usort($source_array, function ($a, $b) {
+            $a_title = $a->get("short_title");
+            $b_title = $b->get("short_title");
+            return strnatcasecmp($a_title, $b_title);
+        });
+    }
+
+    if (count($source_array) > 0) {
+        $grid_widgets[] = array(
             "title" => $title,
-            "full_url" => $full_url
-        ), true);
+            "subtitle" => $subtitle,
+            "tiles" => array_map(function ($x) use ($use_display_title) {
+                return array(
+                    "title" => $use_display_title ? $x->get("display_title") : $x->get("short_title"),
+                    "url_fragment" => $x->get("full_url"),
+                    "thumbnail" => $x->get("thumbnail_photo")
+                );
+            }, $source_array)
+        );
+    }
 
-        ?>
+};
 
-        <?php echo @$widgetBlock; ?>
+function sortByCategoryOrYear($showcasemodels, &$grid_widgets) {
 
-        <div class="inventory-selector-block content_section">
-            <?php
+    // First, you have to figure out if they have categories...
 
-            $grid_widgets = array();
-            function prepare_widget_group($title, $source_array, &$grid_widgets, $use_display_title = false, $sorted_already = false, $subtitle = "") {
-                if (!$sorted_already) {
-                    usort($source_array, function ($a, $b) {
-                        $a_title = $a->get("short_title");
-                        $b_title = $b->get("short_title");
-                        return strnatcasecmp($a_title, $b_title);
-                    });
-                }
+    // sort them by year...
+    $year_buckets = array();
+    $category_buckets = array();
 
-                if (count($source_array) > 0) {
-                    $grid_widgets[] = array(
-                        "title" => $title,
-                        "subtitle" => $subtitle,
-                        "tiles" => array_map(function ($x) use ($use_display_title) {
-                            return array(
-                                "title" => $use_display_title ? $x->get("display_title") : $x->get("short_title"),
-                                "url_fragment" => $x->get("full_url"),
-                                "thumbnail" => $x->get("thumbnail_photo")
-                            );
-                        }, $source_array)
-                    );
-                }
+    foreach ($showcasemodels as $m) {
+        $year = intVal($m->get("year"));
+        if (!array_key_exists($year, $year_buckets)) {
+            $year_buckets[$year] = array();
+        }
+        $year_buckets[$year][] = $m;
 
-            };
-
-            function sortByCategoryOrYear($showcasemodels, &$grid_widgets) {
-
-                // First, you have to figure out if they have categories...
-
-                // sort them by year...
-                $year_buckets = array();
-                $category_buckets = array();
-
-                foreach ($showcasemodels as $m) {
-                    $year = intVal($m->get("year"));
-                    if (!array_key_exists($year, $year_buckets)) {
-                        $year_buckets[$year] = array();
-                    }
-                    $year_buckets[$year][] = $m;
-
-                    $category = $m->get("category");
-                    if (!array_key_exists($category, $category_buckets)) {
-                        $category_buckets[$category] = array();
-                    }
-                    $category_buckets[$category][] = $m;
-                }
+        $category = $m->get("category");
+        if (!array_key_exists($category, $category_buckets)) {
+            $category_buckets[$category] = array();
+        }
+        $category_buckets[$category][] = $m;
+    }
 
 
-                if (count(array_keys($category_buckets)) > 1) {
-                    // display it by the category...
-                    /// sort them...
-                    $category_keys = array_keys($category_buckets);
-                    sort($category_keys);
+    if (count(array_keys($category_buckets)) > 1) {
+        // display it by the category...
+        /// sort them...
+        $category_keys = array_keys($category_buckets);
+        sort($category_keys);
 
-                    // now, display them...
-                    foreach ($category_keys as $c) {
-                        $cat_bucket = $category_buckets[$c];
+        // now, display them...
+        foreach ($category_keys as $c) {
+            $cat_bucket = $category_buckets[$c];
 
-                        usort($cat_bucket, function($a, $b) {
-                            if (intVal($a->get("year")) != intVal($b->get("year"))) {
-                                // newer first...
-                                return (intVal($a->get("year")) > intVal($b->get("year"))) ? -1 : 1;
-                            } else {
-                                return strnatcasecmp($a->get("title"), $b->get("title"));
-                            }
-                        });
-
-                        // we have to put the year on the short list, too.
-                        $clean_bucket = array();
-
-                        $current_year = 0;
-
-                        $title = $c;
-                        foreach ($cat_bucket as $cb) {
-                            if ($current_year != $cb->get("year")) {
-                                if ($current_year > 0 && count($clean_bucket) > 0) {
-                                    prepare_widget_group($title, $clean_bucket, $grid_widgets, false, true, $current_year);
-                                    $title = "";
-                                }
-                                $clean_bucket = array();
-                                $current_year = $cb->get("year");
-                            }
-                            $cb->set("short_title", $cb->get("year") . " " . $cb->get("short_title"));
-                            $clean_bucket[] = $cb;
-                        }
-
-                        if (count($clean_bucket) > 0) {
-                            prepare_widget_group($title, $clean_bucket, $grid_widgets, false, true, $current_year);
-                        }
-                    }
-
+            usort($cat_bucket, function($a, $b) {
+                if (intVal($a->get("year")) != intVal($b->get("year"))) {
+                    // newer first...
+                    return (intVal($a->get("year")) > intVal($b->get("year"))) ? -1 : 1;
                 } else {
-                    $year_bucket_keys = array_keys($year_buckets);
-                    sort($year_bucket_keys);
-                    $year_bucket_keys = array_reverse($year_bucket_keys);
-
-                    foreach ($year_bucket_keys as $year) {
-                        $buckets = $year_buckets[$year];
-
-                        prepare_widget_group($year. " Models", $buckets, $grid_widgets);
-                    }
+                    return strnatcasecmp($a->get("title"), $b->get("title"));
                 }
+            });
+
+            // we have to put the year on the short list, too.
+            $clean_bucket = array();
+
+            $current_year = 0;
+
+            $title = $c;
+            foreach ($cat_bucket as $cb) {
+                if ($current_year != $cb->get("year")) {
+                    if ($current_year > 0 && count($clean_bucket) > 0) {
+                        prepare_widget_group($title, $clean_bucket, $grid_widgets, false, true, $current_year);
+                        $title = "";
+                    }
+                    $clean_bucket = array();
+                    $current_year = $cb->get("year");
+                }
+                $cb->set("short_title", $cb->get("year") . " " . $cb->get("short_title"));
+                $clean_bucket[] = $cb;
             }
 
-
-            if ($display_makes) {
-                prepare_widget_group("", $showcasemakes, $grid_widgets, true);
-            } else if ($display_machine_types) {
-                prepare_widget_group("", $showcasemachinetypes, $grid_widgets);
-            } else if ($display_models) {
-                sortByCategoryOrYear($showcasemodels, $grid_widgets);
-            } else if ($display_trims) {
-                sortByCategoryOrYear($showcasetrims, $grid_widgets);
+            if (count($clean_bucket) > 0) {
+                prepare_widget_group($title, $clean_bucket, $grid_widgets, false, true, $current_year);
             }
+        }
+
+    } else {
+        $year_bucket_keys = array_keys($year_buckets);
+        sort($year_bucket_keys);
+        $year_bucket_keys = array_reverse($year_bucket_keys);
+
+        foreach ($year_bucket_keys as $year) {
+            $buckets = $year_buckets[$year];
+
+            prepare_widget_group($year. " Models", $buckets, $grid_widgets);
+        }
+    }
+}
 
 
-            ?>
+if ($display_makes) {
+    prepare_widget_group("", $showcasemakes, $grid_widgets, true);
+} else if ($display_machine_types) {
+    prepare_widget_group("", $showcasemachinetypes, $grid_widgets);
+} else if ($display_models) {
+    sortByCategoryOrYear($showcasemodels, $grid_widgets);
+} else if ($display_trims) {
+    sortByCategoryOrYear($showcasetrims, $grid_widgets);
+}
 
-            <?php foreach ($grid_widgets as $grid_widget): ?>
-            <div class="showroom-grid-widget <?php if ($grid_widget["title"] == "" && $grid_widget["subtitle"] != ""): ?>subtitleonly<?php endif; ?>">
-                <?php if ($grid_widget["title"] != ""): ?>
-                <div class="showroom-grid-widget-title"><?php echo $grid_widget["title"]; ?></div>
-                <?php endif; ?>
-                <?php if ($grid_widget["subtitle"] != ""): ?>
-                <div class="showroom-grid-widget-subtitle"><?php echo $grid_widget["subtitle"]; ?></div>
-                <?php endif; ?>
+foreach ($grid_widgets as $grid_widget) {
+    mustache_tmpl_iterate($category_selector_v, "grid_widgets");
 
-                <div class="showroom-grid-widget-rows">
-                    <div class="row">
-                    <?php for ($i = 0; $i < count($grid_widget["tiles"]); $i++) {
-                        $tile = $grid_widget["tiles"][$i];
-                        if ($i > 0 && $i % 6 == 0) {
-                            ?></div><div class="row"><?php
-                        }
-                        ?>
-                        <div class="span2 col-md-2 col-sm-4 col-xs-12 showroom-tile-holder" >
-                        <div class=" showroom-tile">
-                            <a href="<?php echo site_url('Factory_Showroom/' . $tile["url_fragment"]); ?>" class="showroom-tile-link"><div class="showroom-tile-image" style="background-image: -webkit-linear-gradient(rgba(255,255,255,0.2) 0%,rgba(0,0,0,0.2) 100%), url('<?php echo (is_null($tile['thumbnail']) || $tile['thumbnail'] == '') ? site_url('/assets/showcase_no_picture_available.png') : $tile['thumbnail']; ?>'); "></div><span class="showroom-tile-title"><?php echo $tile["title"]; ?></span><div class="clear: both"></div></a>
-                        </div>
-                        </div>
-                        <?php
-                    } ?>
+    $transformed_tiles = array();
+    for ($i = 0; $i < count($grid_widget["tiles"]); $i++) {
+        $tile = $grid_widget["tiles"][$i];
+        $new_tile = array(
+            "tile_url" => site_url('Factory_Showroom/' . $tile["url_fragment"]),
+            "tile_title" => $tile["title"],
+            "thumbnail_url" => (is_null($tile['thumbnail']) || $tile['thumbnail'] == '') ? site_url('/assets/showcase_no_picture_available.png') : $tile['thumbnail']
+        );
+        if ($i > 0 && $i % 6 == 0) {
+            $new_tile["next_row"] = true;
+        }
+        $transformed_tiles[] = $new_tile;
+    }
 
-                    </div>
+    mustache_tmpl_iterate($category_selector_v, "grid_widgets", array(
+        "subtitleonly" => $grid_widget["title"] == "" && $grid_widget["subtitle"] != "",
+        "widget_title" => $grid_widget["title"],
+        "widget_subtitle" => $grid_widget["subtitle"],
+        "widget_tiles" => $transformed_tiles
+    ));
+}
 
-                </div>
-
-            </div>
-
-            <?php endforeach; ?>
-
-        </div>
-
-
-        <?php echo @$featureBand; ?>
-
-        <?php echo @$dealsBand; ?>
-
-        <?php echo @$topSellersBand;  ?>
-
-        <?php echo @$recentlyViewedBand; ?>
-
-        <?php if(@$notice && $showNotice): ?>
-            <div class="content_section">
-                <h3><?php echo @$notice; ?></h3>
-            </div>
-        <?php endif; ?>
-
-        <?php if (isset($pageRec) && is_array($pageRec) && array_key_exists("page_custom_js", $pageRec) && $pageRec["page_custom_js"] != ""): ?>
-            <script type="application/javascript">
-                <?php echo $pageRec["page_custom_js"]; ?>
-            </script>
-        <?php endif; ?>
-
-    </div>
-    <!-- END MAIN CONTENT -->
-
-
-    <?php echo @$sidebar; ?>
-
-    <div class="clear"></div>
-
-</div>
-
-<script type="application/javascript">
-    $(window).on("load", function() {
-        $(".showroom-tile-image").css("height", Math.floor($(".showroom-tile-image").width()) + "px");
-    })
-</script>
+print mustache_tmpl_parse($category_selector_v);
