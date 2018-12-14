92283f068920a92eb7330a78e656063edd3e3b92
Jonathan Brown <jonathan@resultsathand.com>
2018-12-14 15:55:06 -0500

    We were having a problem with too many funny characters and it was breaking the feed.

--- application/controllers/abstractadmin/motorcycleadmin.php
+++ application/controllers/abstractadmin/motorcycleadmin.php
@@ -456,7 +456,7 @@ abstract class Motorcycleadmin extends Firstadmin
                     }
                     $arr = array();
                     // JLB 12-14-18: Let us get rid of funny characters.
-                    $img = time() . '_' . preg_replace("/[a-z0-9\.\-]+/i", '_', $val);
+                    $img = time() . '_' . preg_replace("/[^a-z0-9\.\-]+/i", '_', $val);
                     $dir = STORE_DIRECTORY . '/html/media/' . $img;
                     move_uploaded_file($_FILES["file"]["tmp_name"][$key], $dir);
                     $arr['description'] = $_POST['description'];
